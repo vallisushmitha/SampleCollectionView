b49b64a79a83a363f5cbffc52ed6814e90be8cb4
kvanaMini1 <kvanamini1@kvanaMini1s-Mac-mini.local>
2016-07-18 17:05:32 +0530

    collectionView

---
+++ SampleCollectionView/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  SampleCollectionView
+//
+//  Created by kvanaMini1 on 18/05/16.
+//  Copyright © 2016 kvanaMini1. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+}
--- SampleCollectionView/ViewController.swift
+++ SampleCollectionView/ViewController.swift
@@ -8,17 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CollectionViewController: UICollectionViewController {
 
+    
+    var ImageArray = [String]()
+    var LabelArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+         LabelArray = ["Social", "Active", "Artistic", "Dancey", "Family", "Foodie", "Intelligent", "Musical", "Spiritual", "Sporty"]
+        ImageArray = ["mood_social", "mood_active_sel", "mood_artistic", "mood_dancey", "mood_family_sel", "mood_foodie", "mood_intelligent", "mood_musical", "mood_spiritual_sel", "mood_sporty"]
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as UICollectionViewCell
+        
+        
+        let imageView = cell.viewWithTag(1) as! UIImageView
+        
+        imageView.image = UIImage(named: ImageArray[indexPath.row])
+    
+        
+       
+        
+        
+        return cell
+        
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return ImageArray.count
+        
+    }
+    
+
 
 
 }
